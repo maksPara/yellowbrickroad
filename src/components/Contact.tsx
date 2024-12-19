dd2bb51334461728ce79386d9893557473248c0b
Maks Paradowski <mparadowski@griddynamics.com>
2024-12-19 16:59:51 +0100

    side drawer navigation and contact

---
+++ src/components/Contact.tsx
@@ -0,0 +1,33 @@
+const Contact = () => {
+  return (
+    <div className="mt-6 flex flex-col sm:flex-row">
+      <div className="bg-primary flex grow p-4 pt-20">
+        <p className="w-full text-3xl font-bold text-amber-50 sm:text-right">
+          Got questions?
+        </p>
+      </div>
+      <div className="flex grow flex-col gap-2 p-4 pb-10 sm:pt-20">
+        <p className="text-primary mb-10 text-3xl font-bold sm:mb-20">
+          Let's talk
+        </p>
+        <a
+          href="mailto:kacper.holdys@yellowbrickroad.pl"
+          className="link font-bold sm:text-lg"
+        >
+          kacper.holdys@yellowbrickroad.pl
+        </a>
+        <a href="tel:+48602108880" className="link font-bold sm:text-lg">
+          0048 602 108 880
+        </a>
+        <a
+          href="https://calendly.com/kacper-holdys/30min"
+          className="link font-bold sm:text-lg"
+        >
+          book a meeting
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,88 @@
 const Header = () => {
+  const handleClick = () => {
+    document.getElementById('my-drawer')?.click();
+  };
+
   return (
     <header className="navbar sticky top-0 flex justify-center bg-amber-50 p-1 shadow-sm md:shadow-md">
       <div className="max-w-screen-3xl w-full">
         <div className="flex-none md:hidden">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block size-7 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
+          <div className="drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+              {/* Page content here */}
+              <label
+                htmlFor="my-drawer"
+                className="btn btn-square btn-ghost drawer-button"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block size-7 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <ul className="menu text-base-content min-h-full w-80 bg-amber-50 p-4">
+                {/* Sidebar content here */}
+                <li>
+                  <a
+                    className="btn btn-ghost text-base"
+                    href="#about"
+                    onClick={handleClick}
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="btn btn-ghost text-base"
+                    href="#caseStudies"
+                    onClick={handleClick}
+                  >
+                    Case Studies
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="btn btn-ghost text-base"
+                    href="#testimonials"
+                    onClick={handleClick}
+                  >
+                    Testimonials
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="btn btn-ghost text-base"
+                    href="#contact"
+                    onClick={handleClick}
+                  >
+                    Contact
+                  </a>
+                </li>
+                <li className="flex-grow bg-inherit"></li>
+                <li className="btn btn-ghost text-base" onClick={handleClick}>
+                  Close
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+
         <nav className="hidden w-full md:flex md:justify-evenly">
           <a className="btn btn-ghost text-base" href="#about">
             About
@@ -28,9 +92,6 @@ const Header = () => {
           </a>
           <a className="btn btn-ghost text-base" href="#testimonials">
             Testimonials
-          </a>
-          <a className="btn btn-ghost text-base" href="#ourTeam">
-            Our Team
           </a>
           <a className="btn btn-ghost text-base" href="#contact">
             Contact
