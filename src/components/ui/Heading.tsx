b28779b4a4df49ed544169b7ef7df08f2f51499c
Maks Paradowski <mparadowski@griddynamics.com>
2024-12-22 23:23:58 +0100

    add error page

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,5 +1,5 @@
 const Heading = ({ text }: { text: string }) => {
-  return <h2 className="mb-4 text-xl font-bold md:mb-6 md:text-2xl">{text}</h2>;
+  return <h3 className="mb-4 text-xl font-bold md:mb-6 md:text-2xl">{text}</h3>;
 };
 
 export default Heading;
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router';
+
 const NotFoundPage = () => {
-  return <div>404 NOT FOUND</div>;
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="flex grow flex-col items-center justify-center gap-3 bg-amber-50">
+        <h3 className="text-5xl">404</h3>
+        <h3 className="text-xl">Page Not Found</h3>
+        <Link to="/" className="link text-primary">
+          Return home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
